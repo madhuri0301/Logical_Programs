b499cd4e97519d0b1b56d36e4ac552178a0c5059
Madhuri <harshupatil0301@gmail>
2021-05-31 05:57:33 -0700

    Finding Perfect Number

---
+++ Logical_Programs/PerfectNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logical_Programs
+{
+    class PerfectNumber
+    {
+        public int sum = 0;
+        public void Number()
+        {
+            Console.WriteLine("Enter The Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                    sum = sum + i;
+            }
+            if (sum == num)
+                Console.WriteLine(" It is a Perfect Number");
+            else
+                Console.WriteLine("It Is Not a Perfect Number");
+
+        }
+
+    }
+}
+
---
+++ Logical_Programs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Logical_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome TO The Logical Programs");
+
+            Console.WriteLine("1.Perfect Number");
+            Console.WriteLine("2.Prime Number");
+            Console.WriteLine("3.Fibonacci Series");
+            Console.WriteLine("4.Reverse Number");
+
+            Console.WriteLine("Enter The Choice");
+
+            int ch = Convert.ToInt32(Console.ReadLine());
+
+            switch (ch)
+            {
+                case 1:
+                    PerfectNumber perfectnumber = new PerfectNumber();
+                    perfectnumber.Number();
+                    break;
+
+            }
+        }
+    }
+}
